1fd57942b7b6e043eda8b6a64416fb11ffc1c000
zulyanor <zulyano@alterra.id>
2019-07-22 11:12:48 +0700

    first commit django

---
+++ project_model/env_project_model/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta15/Documents/Belajar-CLI/django_mvc/project_model/env_project_model/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ project_views/env_project_views/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta15/Documents/Belajar-CLI/django_mvc/project_views/env_project_views/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
